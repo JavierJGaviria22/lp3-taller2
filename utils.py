56eb0d3b9217ba1a6cd4c98524b34bebee70a859
JavierJGaviria22 <javiergaviria22@gmail.com>
2025-06-02 20:33:45 -0500

    TODO Eliminar caracteres no alfanuméricos (excepto guiones) utils.py

--- utils.py
+++ utils.py
@@ -52,6 +52,7 @@ def generar_slug(texto):
     slug = re.sub(r"\s+", "-", slug)
     
     # TODO: Eliminar caracteres no alfanuméricos (excepto guiones)
+    slug = re.sub(r"[^a-z0-9\-]", "", slug)
     
     # TODO: Reemplazar múltiples guiones con uno solo
     
